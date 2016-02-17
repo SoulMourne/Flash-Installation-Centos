981b922a83db9caca284b8d432b47f0e9b8265ed
SoulMourne <goodwinjeanpaul@yahoo.fr>
2016-02-17 14:27:46 +0100

    Script terminé

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm;
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt;
+rpm -K rpmforge-release-0.5.3-1.el6.rf.*.rpm;
+rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm;
+yum install flash-plugin;
+echo "Terminé" | wall; eject;
