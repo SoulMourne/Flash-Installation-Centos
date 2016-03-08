897544b47bd8a47eb8341d48363653a84b16b841
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-03-08 15:43:14 +0100

    Ne demande plus la confirmation lors de l'envoi

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt;
 rpm -K rpmforge-release-0.5.3-1.el6.rf.*.rpm;
 rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm;
-yum install flash-plugin;
+yum install -y flash-plugin;
 echo "Terminé" | wall; eject;
